6a41182554a450530864b241b675329c0925f00a
Luís Tiago Favas <al62006@utad.eu>
2020-12-22 16:14:04 +0000

    changed rest framework parser

--- backend/hmobiweb/core/settings/base.py
+++ backend/hmobiweb/core/settings/base.py
@@ -108,5 +108,8 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
     ]
 }
