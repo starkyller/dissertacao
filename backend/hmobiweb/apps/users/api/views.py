3f1510b25cf7c9db0151577fad37c8b9a1c4973d
Luís Tiago Favas <al62006@utad.eu>
2020-12-23 17:15:35 +0000

    Updated Logout for post

--- backend/hmobiweb/apps/users/api/views.py
+++ backend/hmobiweb/apps/users/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics, status
@@ -25,7 +27,11 @@ class CustomAuthToken(ObtainAuthToken):
 
 
 class LogoutAPIView(APIView):
-    def get(self, request, format=None):
+    authentication_classes = [TokenAuthentication,]
+    permission_classes = [IsAuthenticated,]
+
+    # def get(self, request, format=None):
+    def post(self, request, *args, **kwargs):
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
 
