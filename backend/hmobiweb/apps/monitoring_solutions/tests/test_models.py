f60e21f156aaff5a9318f8c8e3ce1c5a134054fa
Luís Tiago Favas <al62006@utad.eu>
2020-12-30 15:24:12 +0000

    test models monitoring_solutions

--- backend/hmobiweb/apps/monitoring_solutions/tests/test_models.py
+++ backend/hmobiweb/apps/monitoring_solutions/tests/test_models.py
@@ -18,16 +18,18 @@ class MonitoringCategoryTestCase(TestCase):
 
     def test_db_objects_numbers(self):
         # there should be 6 objects present
-        
+
         obj_count = MonitoringCategory.objects.count()
-        self.assertEqual(obj_count, 6) 
+        self.assertEqual(obj_count, 6)
+
 
 class SolutionObjectiveTestCase(TestCase):
     def setUp(self):
         SolutionObjective.objects.create(designation="Train with Yoda")
 
     def test_properties(self):
-        solutionObj = SolutionObjective.objects.get(designation="Train with Yoda")
+        solutionObj = SolutionObjective.objects.get(
+            designation="Train with Yoda")
 
         self.assertEqual(solutionObj.designation, "Train with Yoda")
         self.assertEqual(solutionObj.__str__(), "Train with Yoda")
@@ -35,6 +37,47 @@ class SolutionObjectiveTestCase(TestCase):
 
     def test_db_objects_numbers(self):
         # there should be 3 objects present
-        
+
         obj_count = SolutionObjective.objects.count()
-        self.assertEqual(obj_count, 3) 
+        self.assertEqual(obj_count, 3)
+
+
+class SolutionTestCase(TestCase):
+    def setUp(self):
+        self._emptyString = ""
+
+        self._monitoringCat = MonitoringCategory.objects.first()  # Health
+        self._solutionObj = SolutionObjective.objects.first()  # Fall Detection
+        self._name = "XPTO FALL"
+        self._description = "The best description ever written"
+        try:
+            solution1 = Solution(
+                name=self._name,
+                description=self._description,
+                category=self._monitoringCat,
+                objective=self._solutionObj,
+            )
+
+            solution1.save()
+
+        except:
+            self.fail('Creation of Address object failed.')
+
+    def test_properties(self):
+        solutionObj = Solution.objects.first()
+
+        self.assertEqual(solutionObj.__str__(), self._name)
+
+        self.assertNotEqual(solutionObj.name, self._emptyString)
+        self.assertEqual(solutionObj.name, self._name)
+
+        self.assertNotEqual(solutionObj.description, self._emptyString)
+        self.assertEqual(solutionObj.description, self._description)
+
+        self.assertNotEqual(
+            solutionObj.category.designation, self._emptyString)
+        self.assertEqual(solutionObj.category.designation, "Health")
+
+        self.assertNotEqual(
+            solutionObj.objective.designation, self._emptyString)
+        self.assertEqual(solutionObj.objective.designation, "Fall Detection")
