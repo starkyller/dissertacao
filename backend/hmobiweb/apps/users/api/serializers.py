ab2d33cf9af8fef5bf0a27e27697395f2969b70d
Luís Tiago Favas <al62006@utad.eu>
2021-02-01 19:45:56 +0000

    debug cleanup

--- backend/hmobiweb/apps/users/api/serializers.py
+++ backend/hmobiweb/apps/users/api/serializers.py
@@ -6,7 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'alias',]
+        fields = ['alias',]
 
 
 class PatientSerializer(serializers.ModelSerializer):
